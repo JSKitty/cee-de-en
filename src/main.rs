022691055e54c9aacf926aeec8d49f980e7ba87f
JSKitty <jskitty@protonmail.com>
2022-11-06 16:22:53 +0000

    Nuke broken JS minifier + cleanup
    Nuked a broken JS minifier (it unfortunately is far too unreliable for large JS files), cleaned up a bunch of comments and code areas and added better file type handling to lock minifying to specific types

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use hyper::{Server, Request, Response, Body, Method, StatusCode};
 use hyper::service::{service_fn, make_service_fn};
 
 use minify_html;
-use minify_js;
 
 use brotlic::{BlockSize, BrotliEncoderOptions, CompressorWriter, Quality, WindowSize};
 
@@ -22,7 +21,7 @@ async fn serve_content(
         // Serve the content for every GET request
         &Method::GET => Ok(
             Response::builder()
-                .status(200)
+                .status(StatusCode::OK)
                 .header("content-encoding", "br")
                 .header("content-type", "text/javascript") // TODO: automate the content-type filling
                 .body(hyper::Body::from((*content).clone())).unwrap()
@@ -44,7 +43,8 @@ async fn main() -> Result<(), std::io::Error> {
     // Note: maybe we want to accept some basic CLI input too?
 
     // TODO: read address and port from config
-    let addr = String::from("0.0.0.0:1337").parse::<SocketAddr>().unwrap();
+    let selected_addr = String::from("0.0.0.0:1337");
+    let addr = selected_addr.parse::<SocketAddr>().unwrap();
 
     // Build the Hyper server
     let svc_builder = make_service_fn(move |_conn| {
@@ -58,20 +58,22 @@ async fn main() -> Result<(), std::io::Error> {
                     let path_string = &req.uri().path().to_string()[1..];
                     let path = PathBuf::from(&path_string);
 
-                    // TODO: Parse the file type into "content-type" suitable strings, we can then use them to
-                    // ... select the correct minifier, decide to compress or not, and also for responding with the correct Content-Type
+                    // Read from resource path (TODO: better handling of missing files, 404s, etc)
+                    let file_contents = fs::read(path).unwrap_or(format!("Nope, no {path_string} found here m8").into_bytes());
 
-                    // Read from resource path (TODO: better handling of missing files)
-                    let file_contents = fs::read(path).unwrap_or(format!("Naw the file {path_string} don't exist here m8").into_bytes());
+                    // First: Minify! (if applicable)
+                    let minified: Vec<u8>;
+                    if path_string.ends_with(".html") || path_string.ends_with(".js") || path_string.ends_with(".css") {
+                        // HTML minify (TODO: improve JS minifying with a dedicated lib or custom function, also add comment removal somehow)
+                        let mut cfg = minify_html::Cfg::new();
+                        cfg.keep_comments = false;
+                        minified = minify_html::minify(&file_contents, &cfg);
+                    } else {
+                        // We just pass along the original file contents, nothing to minify!
+                        minified = file_contents.clone();
+                    }
 
-                    // First: we minify anything code-wise
-                    // TODO: only minify [js, html, css]
-                    let mut minify_cfg = minify_html::Cfg::new();
-                    minify_cfg.keep_comments = false;
-
-                    let minified = minify_html::minify(&file_contents, &minify_cfg);
-
-                    // Second: we apply Brotli compression
+                    // Second: Brotli compression!
                     // TODO: move the encoder options outside of the service scope (pre-load) and load config values set by the user, if set.
                     let encoder = BrotliEncoderOptions::new()
                         .quality(Quality::best())
@@ -89,7 +91,7 @@ async fn main() -> Result<(), std::io::Error> {
                     // For science: log the benefits we achieved from the above steps!
                     println!("Resource was [{}] bytes, reduced to [{}] via minifying and then [{}] by Brotli", file_contents.len(), minified.len(), compressed_file.len());
                     
-                    // Capture the file contents in an Arc so we can use the reference repeatedly
+                    // Capture the return bytes in an Arc so we can use the reference repeatedly
                     // across async tasks that the server will spawn
                     let resource_finalised = Arc::new(compressed_file);
 
@@ -101,7 +103,7 @@ async fn main() -> Result<(), std::io::Error> {
     });
 
     // Start up our service and accept connections
-    println!("Starting server...");
+    println!("Starting server at interface '{}'...", selected_addr);
     let server = Server::bind(&addr).serve(svc_builder);
     if let Err(e) = server.await {
         eprintln!("Server error: {}", e);
