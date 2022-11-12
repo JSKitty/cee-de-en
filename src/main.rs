90390bcc23fd8cf4174f2541ac7cefd2db9c6de4
JSKitty <jskitty@protonmail.com>
2022-11-12 17:25:48 +0000

    Basic Cache + alloc optimisations A super basic, pretty flawed cache system has been added, also with some huge allocation optimisations (like no longer needing to clone the entire file in memory multiple times during certain logic flows!).

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use std::net::SocketAddr;
 use std::convert::Infallible;
 use std::sync::Arc;
+use std::collections::HashMap;
 
 use hyper::{Server, Request, Response, Body, Method, StatusCode};
 use hyper::service::{service_fn, make_service_fn};
@@ -48,6 +49,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     // Build the Hyper server
     let svc_builder = make_service_fn(move |_conn| {
+        // Create our hit caches
+        let mut processed_cache: HashMap<String, Arc<Vec<u8>>> = HashMap::new();
         async {
             // Create our 'CDN' endpoint which essentially just assumes the request path to be a *relative* disk path
             Ok::<_, Infallible>(
@@ -57,46 +60,63 @@ async fn main() -> Result<(), std::io::Error> {
                     // Parse the resource path (chopping off the initial `/`)
                     let path_string = &req.uri().path().to_string()[1..];
                     let path = PathBuf::from(&path_string);
+                    let path_key = path.clone().to_string_lossy().to_string();
 
                     // Read from resource path (TODO: better handling of missing files, 404s, etc)
-                    let file_contents = fs::read(path).unwrap_or(format!("Nope, no {path_string} found here m8").into_bytes());
-
-                    // First: Minify! (if applicable)
-                    let minified: Vec<u8>;
-                    if path_string.ends_with(".html") || path_string.ends_with(".js") || path_string.ends_with(".css") {
-                        // HTML minify (TODO: improve JS minifying with a dedicated lib or custom function, also add comment removal somehow)
-                        let mut cfg = minify_html::Cfg::new();
-                        cfg.keep_comments = false;
-                        minified = minify_html::minify(&file_contents, &cfg);
+                    let file_contents: Vec<u8>;
+                    let cache_hit = processed_cache.contains_key(&path_key);
+                    
+                    // Return from hit cache with zero pre-processing (extreme speed), or load from disk and process on-demand (slow)
+                    if cache_hit {
+                        file_contents = (*processed_cache.get(&path_key).unwrap()).to_vec();
                     } else {
-                        // We just pass along the original file contents, nothing to minify!
-                        minified = file_contents.clone();
+                        file_contents = fs::read(path.clone()).unwrap_or(format!("Nope, no {path_string} found here m8").into_bytes());
                     }
 
-                    // Second: Brotli compression!
-                    // TODO: move the encoder options outside of the service scope (pre-load) and load config values set by the user, if set.
-                    let encoder = BrotliEncoderOptions::new()
-                        .quality(Quality::best())
-                        .window_size(WindowSize::best())
-                        .block_size(BlockSize::best())
-                        .build().unwrap();
+                    // All operations in the non-cache block are for uncached resource only
+                    if cache_hit {
+                        println!("Resource served instantly from cache!");
+                    } else {
+                        // First: Minify! (if applicable)
+                        let mut minified: Vec<u8> = Vec::new();
 
-                    let compressor_storage = Vec::new();
-                    let mut compressed_writer = CompressorWriter::with_encoder(encoder, compressor_storage);
+                        // TODO: move to MIME-type comparisons, not strings?
+                        if path_string.ends_with(".html") || path_string.ends_with(".js") || path_string.ends_with(".css") {
+                            // HTML minify (TODO: improve JS minifying with a dedicated lib or custom function, also add comment removal somehow)
+                            let mut cfg = minify_html::Cfg::new();
+                            cfg.keep_comments = false;
+                            minified = minify_html::minify(&file_contents, &cfg);
+                        }
 
-                    // TODO: catch any weird compression errors and fallback to raw file (why would these happen?)
-                    compressed_writer.write_all(minified.as_slice()).unwrap();
-                    let compressed_file = compressed_writer.into_inner().unwrap();
+                        // Second: Brotli compression!
+                        // TODO: move the encoder options outside of the service scope (pre-load) and load config values set by the user, if set.
+                        let encoder = BrotliEncoderOptions::new()
+                            .quality(Quality::best())
+                            .window_size(WindowSize::best())
+                            .block_size(BlockSize::best())
+                            .build().unwrap();
+                        let mut compressed_writer = CompressorWriter::with_encoder(encoder, Vec::new());
 
-                    // For science: log the benefits we achieved from the above steps!
-                    println!("Resource was [{}] bytes, reduced to [{}] via minifying and then [{}] by Brotli", file_contents.len(), minified.len(), compressed_file.len());
-                    
-                    // Capture the return bytes in an Arc so we can use the reference repeatedly
-                    // across async tasks that the server will spawn
-                    let resource_finalised = Arc::new(compressed_file);
+                        // TODO: catch any weird compression errors and fallback to raw file (why would these happen?)
+                        let compressed_file: Vec<u8>;
+                        if minified.len() > 0 {
+                            compressed_writer.write_all(minified.as_slice()).unwrap();
+                            compressed_file = compressed_writer.into_inner().unwrap();
+                            println!("Resource was [{}] bytes, reduced to [{}] via minifying and then [{}] by Brotli", file_contents.len(), minified.len(), compressed_file.len());
+                        } else {
+                            compressed_writer.write_all(file_contents.as_slice()).unwrap();
+                            compressed_file = compressed_writer.into_inner().unwrap();
+                            println!("Resource was [{}] bytes, reduced to [{}] by Brotli", minified.len(), compressed_file.len());
+                        }
+
+                        // Capture the return bytes in an Arc so we can use the reference repeatedly
+                        // across async tasks that the server will spawn, then push the Arc in to our
+                        // cache memory.
+                        processed_cache.insert(path_key, Arc::new(compressed_file));
+                    }
 
                     // Serve the compressed bytes!
-                    serve_content(req, resource_finalised)
+                    serve_content(req, processed_cache.get(&path.to_string_lossy().to_string()).unwrap().to_owned())
                 })
             )
         }
